a53eb8283f640a3bcf2221a13c7b0bb390bc972a
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-02-27 19:46:34 -0800

    added new pages

---
+++ javascripts/stripes.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+	var divisions = random([5, 35]);//set tiles to odd number
+	if ( divisions % 2 === 0 ) {
+		divisions += 1;
+	}
+
+//set up default css and html
+	$('body').css('margin', 0).css('padding', 0);
+	for ( var i = 0; i < divisions; i++ ) {
+		var div = $(document.createElement('div'));
+		$('body').append(div);
+	}
+	$('div').css('float', 'left');
+	var frameRate = random([30, 100]);
+
+	var displayFrame = frame(divisions);
+
+	setInterval(function() {
+		displayFrame();
+	}, frameRate );
+});
+
+var frame = function(divisions) {
+	var height = $(window).height();
+	var width = $(window).width();
+	var divHeight = height / divisions;
+	var divWidth = width / divisions;
+	var counter = 0;
+	var orientation = Math.floor(Math.random() * 2);
+	var backgroundColor = {
+		color: 'white',
+		toggle: function() {
+			this.color = this.color === 'white' ? 'black' : 'white';
+		}
+	};
+
+	// var increment = function() {
+	// 	counter = (counter + 1) % 2;
+	// 	return counter;
+	// };
+
+	return function() {
+		var $divs = $('div');
+		$divs.each(function() {
+			backgroundColor.toggle();
+			$(this).css('background-color', backgroundColor.color);
+		});
+		switch(orientation) {
+		case 0://horizontal
+			$('div').css('height', divHeight);
+			$('div').css('width', width);
+			break;
+		case 1://vertical
+			$('div').css('height', height);
+			$('div').css('width', divWidth);
+			break;
+		}
+	};
+};
+
+var random = function(range) {
+	return Math.floor(Math.random() * (range[1] - range[0]) + range[0]);
+};
